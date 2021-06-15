0957fdd816bb991dca45341eea798c5387a3122c
ar-boop <83057745+ar-boop@users.noreply.github.com>
2021-06-15 22:30:53 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AllCardsComponent } from './all-cards/all-cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AllCardsComponent
   ],
   imports: [
     BrowserModule,
