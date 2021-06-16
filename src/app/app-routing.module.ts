2bb903e9124ba585dd12a4a8215d2d678a1f5466
ar-boop <83057745+ar-boop@users.noreply.github.com>
2021-06-16 09:13:32 +0530

    changed routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AllCardsComponent } from './all-cards/all-cards.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path:'',redirectTo:'home',pathMatch:'full'},
   {path:'home',component:HomeComponent},
   {path:'allcards',component:AllCardsComponent}];
 
